abda0beda9f584f721de561a7affe901875aeb4e
frager5234 <mmoshin22224@gmail.com>
2024-07-09 23:08:52 +0700

    Add files via upload

---
+++ oop/Worker.php
@@ -0,0 +1,69 @@
+<?php
+//class worker
+//{
+//    public $name = "Имя";
+//    public $age = "Возраст";
+//    public $salary = "ZP";
+//
+//
+//    public function getAge()
+//    {
+//        echo "Ivan {$this->age} 25, {$this->salary} 1000     ";
+//    }
+//
+//    public function getAgeVasya()
+//    {
+//        echo "Vasya {$this->age} 26, {$this->salary} 2000    ";
+//    }
+//
+//}
+//
+//$admin = new worker();
+//$admin1 = new worker();
+//
+//
+//$admin->getAge();
+//$admin1->getAgeVasya();
+//
+//
+//
+//$a = array(25, 26);
+//echo "Сумма возраста " . array_sum($a) . "\n";
+//
+//$b = array(1000, 2000);
+//echo "Сумма зп  = " . array_sum($b) .
+
+
+class Worker
+{
+    public $name;
+    public $age;
+    public $salary;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
